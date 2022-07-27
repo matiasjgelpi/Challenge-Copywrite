0b65c78b41da4dfd23ed15efef56373f18ea8f1d
matiasjgelpi <matiasjgelpi@gmail.com>
2022-07-27 18:45:55 -0300

    react app created

--- api/index.js
+++ api/index.js
@@ -11,8 +11,8 @@ app.get('/iecho', reverseText)
 
 app.use('/', noEndpointReached)
 
-const server = app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+const server = app.listen(4000, () => {
+  console.log('Server is running on port 4000')
 })
 
 module.exports = { app, server }
